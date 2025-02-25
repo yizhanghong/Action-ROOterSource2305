aab8703ff29451e3146f6e97d1aa96371a5a48ea
yizhanghong <865362414@qq.com>
2025-02-25 13:57:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.14.1/g' package/base-files/files/bin/config_generate
 sed -i '/CONFIG_PACKAGE_glib2=y/d' .config
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
+#make package/mosdns/luci-app-mosdns/compile V=s
